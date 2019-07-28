4b42be7e02da8c29c2449edcea9003c561827c34
Alvin10919058 <zero840108@gmail.com>
2019-07-29 04:02:39 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,378 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="top-section">
+      <div class="col-1 put-area mlr-1">
+        <div class="ps-r shaddow" draggable="true">
+          <img src="./assets/img/Card_spade A.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_spade 2.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_spade 3.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_spade 4.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_spade 5.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_spade 6.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_spade 7.svg" alt="">
+        </div>
+      </div>
+      <div class="col-2 put-area mlr-1">
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_spade 8.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_spade 9.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_spade 10.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_spade J.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_spade Q.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_spade K.svg" alt="">
+        </div>
+      </div>
+      <div class="col-3 put-area mlr-1">
+        <div class="ps-r shaddow" draggable="true">
+          <img src="./assets/img/Card_heart A.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_heart 2.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_heart 3.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_heart 4.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_heart 5.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_heart 6.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_heart 7.svg" alt="">
+        </div>
+      </div>
+      <div class="col-4 put-area mlr-1">
+         <div class="ps-r shaddow">
+          <img src="./assets/img/Card_heart 8.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_heart 9.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_heart 10.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_heart J.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_heart Q.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_heart K.svg" alt="">
+        </div>
+      </div>
+      <div class="col-5 put-area mlr-1">
+        <div class="ps-r shaddow" draggable="true">
+          <img src="./assets/img/Card_club A.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_club 2.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_club 3.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_club 4.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_club 5.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_club 6.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_club 7.svg" alt="">
+        </div>
+      </div>
+      <div class="col-6 put-area mlr-1">
+         <div class="ps-r shaddow">
+          <img src="./assets/img/Card_club 8.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_club 9.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_club 10.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_club J.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_club Q.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_club K.svg" alt="">
+        </div>
+      </div>
+      <div class="col-7 put-area mlr-1">
+        <div class="ps-r shaddow" draggable="true">
+          <img src="./assets/img/Card_diamond A.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_diamond 2.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_diamond 3.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_diamond 4.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_diamond 5.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_diamond 6.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_diamond 7.svg" alt="">
+        </div>
+      </div>
+      <div class="col-8 put-area mlr-1">
+         <div class="ps-r shaddow">
+          <img src="./assets/img/Card_diamond 8.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_diamond 9.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_diamond 10.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_diamond J.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_diamond Q.svg" alt="">
+        </div>
+        <div class="ps-r shaddow">
+          <img src="./assets/img/Card_diamond K.svg" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="bottom-section">
+      <div class="temp-stack-container">
+        <div class="put-area"></div>
+        <div class="put-area"></div>
+        <div class="put-area"></div>
+        <div class="put-area"></div>
+      </div>
+      <div class="game-panel">
+        <div class="btn-box">
+          <div class="undo"></div>
+          <div class="hint"></div>
+          <div class="setting"></div>
+        </div>
+        <div class="info-box">
+          <div class="info">Score | 0</div>
+          <div class="info">Time &nbsp;| 00:00</div>
+        </div>
+      </div>
+      <div class="goal-stack-container">
+        <div class="put-area goal">A</div>
+        <div class="put-area goal">A</div>
+        <div class="put-area goal">A</div>
+        <div class="put-area goal">A</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  data() {
+    return {};
+  },
+  mounted() {
+    for (let i = 1; i <= 8; i++) {
+      document
+        .querySelectorAll(`div.col-${i} > div.shaddow > img`)
+        .forEach((e, index) => {
+          e.style = `top:${-5 + 38 * index}px`;
+        });
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
+html,
+body {
+  margin: 0px;
+  width: 100%;
+  height: 100%;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
+  background-image: url("./assets/img/background-Start.svg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  .put-area {
+    border: 2px solid #fff;
+    border-radius: 13px;
+    width: 103px;
+    height: 138px;
+  }
+  .mlr-1 {
+    margin: 0px 10px;
+  }
+  .top-section {
+    height: 75%;
+    box-sizing: border-box;
+    display: flex;
+    padding-top: 20px;
+    justify-content: center;
+  }
+  .bottom-section {
+    height: 24%;
+    display: flex;
+    .goal {
+      font-size: 48px;
+      color: rgb(167, 193, 212);
+      font-weight: 900;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .temp-stack-container {
+      display: flex;
+      justify-content: space-around;
+      width: 40%;
+      padding: 10px 10px 10px 30px;
+    }
+    .game-panel {
+      width: 20%;
+      padding: 37px 10px 10px 10px;
+      .btn-box {
+        display: flex;
+        justify-content: space-around;
+        .undo {
+          width: 40px;
+          height: 40px;
+          cursor: pointer;
+          background-image: url("./assets/img/element-14.svg");
+          background-repeat: no-repeat;
+          background-position: center;
+        }
+        .hint {
+          width: 40px;
+          height: 40px;
+          cursor: pointer;
+          background-image: url("./assets/img/element-15.svg");
+          background-repeat: no-repeat;
+          background-position: center;
+        }
+        .setting {
+          width: 40px;
+          height: 40px;
+          cursor: pointer;
+          background-image: url("./assets/img/element-16.svg");
+          background-repeat: no-repeat;
+          background-position: center;
+        }
+      }
+      .info-box {
+        padding: 7px;
+        .info {
+          text-align: left;
+          width: 135px;
+          font-size: 22px;
+          font-weight: 900;
+          color: #294254;
+          margin: 0px auto;
+        }
+      }
+    }
+    .goal-stack-container {
+      display: flex;
+      justify-content: space-around;
+      width: 40%;
+      padding: 10px 30px 10px 10px;
+    }
+  }
 }
+.shaddow {
+  filter: drop-shadow(0px -0.5px 3px rgba(0, 0, 0, 0.7));
+}
+.ps-r {
+  position: relative;
+  img {
+    position: absolute;
+    top: -6px;
+    left: -4px;
+    width: 112px;
+  }
+}
+// .card {
+//   background-repeat: no-repeat;
+//   background-position: -3px -2px;
+//   border-radius: 10px;
+//   width: 104px;
+//   height: 144px;
+//   background-size: cover;
+// }
+// .c1 {
+//   // background-image: url("./assets/img/Card_spade A.svg");
+//   // background-repeat: no-repeat;
+//   // background-position: -4px -4px;
+//   // border-radius: 5px;
+//   // width: 100px;
+//   // height: 144px;
+//   // background-size: cover;
+//   background-image: url("./assets/img/Card_spade A.svg");
+// }
+// .c2 {
+//   background-image: url("./assets/img/Card_spade 2.svg");
+//   // background-repeat: no-repeat;
+//   // background-position: -4px -4px;
+//   // border-radius: 5px;
+//   // width: 100px;
+//   // height: 144px;
+//   // background-size: cover;
+// }
+// .card-3 {
+//   background-image: url("./assets/img/Card_spade 3.svg");
+//   background-repeat: no-repeat;
+//   background-position: -4px -4px;
+//   border-radius: 5px;
+//   width: 100px;
+//   height: 144px;
+//   background-size: cover;
+// }
 </style>
